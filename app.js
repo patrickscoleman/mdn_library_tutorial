33b63ba5d6fb6d92c82205343514e566e97825d0
Patrick Coleman <patrickscoleman@gmail.com>
2022-12-04 20:55:34 -0800

    Add favicon

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
